f384542f0a5a6d9838a4678fc79e7b000cf036da
indranyeve <indranyeve@gmail.com>
2021-03-11 02:28:41 -0400

    first commit

---
+++ ProyectoInventario/connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoInventario
+{
+    public class connection
+    {
+        public static SqlConnection getConnection()
+        {
+            SqlConnection ocon = new SqlConnection("Data Source=DESKTOP-3AO6C47;Initial Catalog=BaseDatosInventario;Integrated Security=True");
+            ocon.Open();
+            return ocon;
+        }
+    }
+}
